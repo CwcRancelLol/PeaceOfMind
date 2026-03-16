474c2306b7fa2c4e1b86f198c0a358942e63e786
Corporan Lagombra <1323815@bcpss.org>
2026-03-16 12:45:15 -0400

    idk

--- app/src/main/java/com/example/peaceofmind/MainActivity.java
+++ app/src/main/java/com/example/peaceofmind/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.peaceofmind;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +15,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button beginBTN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        beginBTN = (Button)findViewById(R.id.beginBTN);
+
+
+
+
     }
+
+    beginBTN.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick (View v){
+
+        Intent leaveIntent = new Intent(MainActivity.this, activity_emoji.class);
+        startActivity(leaveIntent);
+
+    }
+
+
+    });
 }
